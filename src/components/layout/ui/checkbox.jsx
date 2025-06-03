3894f7b482ba6e80b4597a4c75ab9177fd1dc20a
VikramDevireddy <vikramdevireddy888@gmail.com>
2025-06-03 10:03:13 +0530

    aaa

--- src/components/layout/ui/checkbox.jsx
+++ src/components/layout/ui/checkbox.jsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
 import { Check } from "lucide-react"
-import { cn } from "../../../util/cn"
+import { cn } from "../../../utils/cn"
 
 const Checkbox = React.forwardRef(({ className, ...props }, ref) => (
   <CheckboxPrimitive.Root
--- src/components/ui/badge.jsx
+++ src/components/ui/badge.jsx
@@ -1,5 +1,5 @@
 import { cva } from "class-variance-authority"
-import { cn } from "../../utils/cn"
+import { cn } from "../../util/cn"
 const badgeVariants = cva(
   "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
   {
