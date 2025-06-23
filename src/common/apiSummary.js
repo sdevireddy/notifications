099b0f2974d6568b6d11913ac4288b9173984189
sivasankara.rao <sivasankara.rao@saviynt.com>
2025-06-23 17:34:02 +0530

    api change

--- src/common/apiSummary.js
+++ src/common/apiSummary.js
@@ -1,10 +1,15 @@
-export const baseUrl="http://ec2-13-49-44-42.eu-north-1.compute.amazonaws.com:8081/api"
+//export const baseUrl="http://ec2-13-49-44-42.eu-north-1.compute.amazonaws.com:8081/"
+export const baseUrl="http://localhost:8081/auth"
 
 
 
 export const apiSummary={
     login:{
         url:"/login",
+        method:"post",
+    },
+    register:{
+        url:"/createAccount",
         method:"post",
     },
     createLeads:{
