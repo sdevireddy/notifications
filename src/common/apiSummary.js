35cde332bfdce7d192be0e7c2dfbfa542ebe5c1e
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-07-13 10:56:38 +0530

    ingetrated login api

--- src/common/apiSummary.js
+++ src/common/apiSummary.js
@@ -1,5 +1,5 @@
 //export const baseUrl="http://ec2-13-49-44-42.eu-north-1.compute.amazonaws.com:8081/"
-export const baseUrl="https://ec2-16-171-41-109.eu-north-1.compute.amazonaws.com:8443/"
+export const baseUrl="http://ec2-16-171-41-109.eu-north-1.compute.amazonaws.com:8443/"
 
 
 
