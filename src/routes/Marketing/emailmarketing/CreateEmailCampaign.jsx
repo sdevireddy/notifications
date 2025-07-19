a41121bea665bc343bd6993c50b92fc490aa7664
VikramDevireddy <vikramdevireddy888@gmail.com>
2025-07-19 21:22:39 +0530

    .

--- src/routes/Marketing/emailmarketing/CreateEmailCampaign.jsx
+++ src/routes/Marketing/emailmarketing/CreateEmailCampaign.jsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { Input } from "../../../components/ui/input";
-import { Button } from "../../../components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import { format } from "date-fns";
 
 import {
