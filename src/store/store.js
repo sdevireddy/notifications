576556c0f91d140bf2452fd579821165d4addfb1
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2025-07-30 21:16:09 +0530

    restict modules in crm

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import orgReducer from './orgSlice'
 import userReducer from "./userSlice"
+import moduleReducer from "./moduleSlice"
 export const store = configureStore({
   reducer: {
     organization:orgReducer,
-    user:userReducer
+    user:userReducer,
+    module:moduleReducer,
   },
   devTools:true,
 })
